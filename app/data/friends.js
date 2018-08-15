bcf8767e7bf4e819b560bc4f39b1b13b87dee11b
kharrison34 <33838826+kharrison34@users.noreply.github.com>
2018-08-15 17:53:09 -0400

    Update friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -176,25 +176,3 @@ var friendsArray = [
 
 module.exports = friendsArray;
 
-
-// var tableArray = [
-//   {
-//     "name":"Jude",
-//     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//     "scores":[
-//         5,
-//         1,
-//         4,
-//         4,
-//         5,
-//         1,
-//         2,
-//         5,
-//         4,
-//         1
-//       ]
-//   }  
-//   ];
-  
-//   // Note how we export the array. This makes it accessible to other files using require.
-//   module.exports = tableArray;
